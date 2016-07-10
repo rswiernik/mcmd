68c44f968aac9fa11cd34e529c012cd525865dc9
Reed Swiernik <rswiernik@csh.rit.edu>
2016-07-10 16:42:34 -0400

    Initial Commit

---
+++ src/mcmd.py
@@ -0,0 +1,12 @@
+# File: src/mcmd.py
+# Description:
+#   This file contains the source for the (M)ine(c)raft (M)anagement (D)aemon
+
+import os
+import socket
+import time
+import argparse
+import logging
+import grp
+
+
